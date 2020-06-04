38ebcb6488290f8dad08a5e98708554daf7f3b6c
Didier <didierpg@gmail.com>
2020-06-04 01:42:54 -0300

    Dashboard

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -70,6 +70,7 @@ export const TableContainer = styled.section`
       font-size: 16px;
       font-weight: normal;
       color: #969cb3;
+      white-space: nowrap;
 
       &.title {
         color: #363f5f;
